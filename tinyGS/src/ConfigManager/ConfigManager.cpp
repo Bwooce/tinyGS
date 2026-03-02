617d6d44fb8be25ae810dddf11bdaecd972d6465
Bruce Fitzsimons <bruce@fitzsimons.org>
2026-03-02 13:51:48 +1100

    feat(dashboard): add comprehensive power telemetry and real WiFi RSSI

--- tinyGS/src/ConfigManager/ConfigManager.cpp
+++ tinyGS/src/ConfigManager/ConfigManager.cpp
@@ -243,7 +243,7 @@ void ConfigManager::handleDashboard()
   s += "<tr><td>Name </td><td>" + String(getThingName()) + "</td></tr>";
   s += "<tr><td>Version </td><td>" + String(status.version) + "</td></tr>";
   s += "<tr><td>MQTT Server </td><td>" + String(status.mqtt_connected ? "<span class='G'>CONNECTED</span>" : "<span class='R'>NOT CONNECTED</span>") + "</td></tr>";
-  s += "<tr><td>WiFi RSSI </td><td>" + String(WiFi.isConnected() ? "<span class='G'>CONNECTED</span>" : "<span class='R'>NOT CONNECTED</span>") + "</td></tr>";
+  s += "<tr><td>WiFi RSSI </td><td>" + (WiFi.isConnected() ? String(WiFi.RSSI()) + " dBm" : "<span class='R'>DISCONNECTED</span>") + "</td></tr>";
   s += "<tr><td>Radio </td><td>" + String(Radio::getInstance().isReady() ? "<span class='G'>READY</span>" : "<span class='R'>NOT READY</span>") + "</td></tr>";
   
   if (!GnssManager::getInstance().isEnabled()) {
@@ -256,9 +256,21 @@ void ConfigManager::handleDashboard()
       s += "<tr><td>GNSS </td><td><span class='R'>NO FIX (" + String(GnssManager::getInstance().getSatellites()) + ")</span></td></tr>";
   }
 
-  float battVol = Power::getInstance().getBatteryVoltage();
-  int battPct = Power::getInstance().getBatteryPercentage();
-  s += "<tr><td>Battery </td><td>" + (battVol > 0 ? String(battVol/1000.0, 2) + "V (" + String(battPct) + "%)" : String(" - ")) + "</td></tr>";
+  Power& power = Power::getInstance();
+  float battVol = power.getBatteryVoltage();
+  int battPct = power.getBatteryPercentage();
+  float battCur = power.getBatteryCurrent();
+  bool charging = power.isCharging();
+  bool vbus = power.isVbusPresent();
+
+  s += "<tr><td>Power Source </td><td>" + String(vbus ? "USB / VBUS" : "Battery") + "</td></tr>";
+  if (vbus) {
+      s += "<tr><td>Charging </td><td>" + String(charging ? "<span class='G'>YES</span>" : "NO") + "</td></tr>";
+  }
+  s += "<tr><td>Battery </td><td>" + (battVol > 100 ? String(battVol/1000.0, 2) + "V (" + String(battPct) + "%)" : String(" - ")) + "</td></tr>";
+  if (battVol > 100) {
+      s += "<tr><td>Current </td><td>" + String(battCur, 1) + " mA (" + (battCur >= 0 ? "Charging" : "Discharging") + ")</td></tr>";
+  }
   
   s += F("</table></div>");
 
--- tinyGS/src/Power/Power.h
+++ tinyGS/src/Power/Power.h
@@ -54,12 +54,17 @@
 #define AXP2101_BATTERY_VOLT_L      0x35
 #define AXP2101_VBUS_VOLT_H         0x38
 #define AXP2101_VBUS_VOLT_L         0x39
+#define AXP2101_BATT_DISCHG_CUR_H   0x3C
+#define AXP2101_BATT_DISCHG_CUR_L   0x3D
+#define AXP2101_BATT_CHG_CUR_H      0x3E
+#define AXP2101_BATT_CHG_CUR_L      0x3F
 #define AXP2101_FUEL_GAUGE          0xA4
 
 #define AXP2101_BATT_VOLT_MASK      0xFF
 #define AXP2101_BATT_VOLT_SHIFT     8
 #define AXP2101_VBUS_VOLT_MASK      0xFF
 #define AXP2101_VBUS_VOLT_SHIFT     8
+#define AXP2101_CUR_MASK            0x3FFF // 14 bits
 
 extern Status status;
 
@@ -74,6 +79,9 @@ public:
      float getBatteryVoltage();
      int getBatteryPercentage();
      float getVbusVoltage();
+     bool isVbusPresent();
+     bool isCharging();
+     float getBatteryCurrent();
      void setGnssPower(bool on);
      void deepSleepSensors();
      TwoWire* getPmuWire() { return pmuWire; }
